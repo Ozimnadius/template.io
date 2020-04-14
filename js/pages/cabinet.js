876645c9fd90703e01595fd0bf18505a0668b704
font-size: 16px <A7rty49Z>
2020-04-14 11:20:30 +0300

    lk-data.html - adaptive,PxPf,validation

--- js/pages/cabinet.js
+++ js/pages/cabinet.js
@@ -2,6 +2,7 @@
 $(function () {
 
     // VARIABLES
+    const $accinput = $('.accpayments__input');
 
 
     // EVENTS
@@ -13,6 +14,22 @@ $(function () {
         $order.toggleClass('active');
         $bottom.slideToggle();
 
+    });
+
+    //ввод цифр в поля
+    $('.accpayments__input').on('keypress', function (e) {
+        if (e.key.match(/[^0-9]/g) || (this.value == 0 && e.key == 0)) {
+            e.preventDefault();
+        }
+    });
+
+    //Фиксированный платеж
+    $('.accpayments__item').on('click', function (e) {
+        e.preventDefault();
+        let $this= $(this),
+            val = parseInt($this.text());
+
+        $accinput.val(val);
     });
 
 
